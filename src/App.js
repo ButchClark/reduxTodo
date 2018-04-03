414f8ff35f9228b4af4199e8f74a02711a5b8041
Butch Clark <butch.clark@hushmail.com>
2018-04-03 06:46:39 -0600

    working thru tutorial

--- src/App.js
+++ src/App.js
@@ -3,7 +3,11 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+
   render() {
+
+
     return (
       <div className="App">
         <header className="App-header">
--- src/index.js
+++ src/index.js
@@ -4,9 +4,23 @@ import {Provider} from 'react-redux'
 import {createStore} from 'redux'
 import rootReducer from './reducers'
 import App from './components/App'
+import {addTodo,toggleTodo,setVisibilityFilter,VisibilityFilters} from "./actions";
 
 const store = createStore(rootReducer)
+console.log(`store.getState(): ${store.getState()}`)
 
+const unsubscirbe = store.subscribe(()=>{
+    console.log(store.getState())
+})
+
+store.dispatch(addTodo('Learn about actions'))
+store.dispatch(addTodo('Learn about reducers'))
+store.dispatch(addTodo('Learn about store'))
+store.dispatch(toggleTodo(0))
+store.dispatch(toggleTodo(1))
+store.dispatch(setVisibilityFilter(VisibilityFilters.SHOW_COMPLETED))
+
+unsubscirbe()
 render(
   <Provider store={store}>
 	<App />
